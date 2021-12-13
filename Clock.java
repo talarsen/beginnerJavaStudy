6f79976f40d7aadbc7766e28920d164fdac6a515
Tara Larsen <talarsen74@gmail.com>
2021-12-12 17:12:46 -0800

    initial commit

---
+++ Clock.java
@@ -0,0 +1,10 @@
+class Clock {
+//declare String here not char becuase char is only one character. 
+ String time;
+ void setTime(String t) {
+ time = t;
+ }
+ String getTime() {
+ return time;
+ }
+}
